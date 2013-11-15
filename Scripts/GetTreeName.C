8493e26c85d2ada58c1b5bac50286e53780e9ae0
Pawandeep <pjand001@ucr.edu>
2013-11-14 16:05:02 -0800

    update file

--- Scripts/GetTreeName.C
+++ Scripts/GetTreeName.C
@@ -1,14 +1,11 @@
-
-
-//cfA version hardcoded!!
+//Careful cfA versions are hardcoded..
 void GetTreeName( TString inputFile="") {
-//reducedTree.CSVM_PF2PATjets_JES0_JER0_PFMETTypeI_METunc0_PUunc0_BTagEff05_HLTEff0.SMS-MadGraph_T1t1t-1000-800to800-100to550_UCSB1754reshuf_v68_1.root
 
   string str;
   string name( inputFile );
   unsigned int pos = name.find_last_of("/");
 
-  //Get name of file only not the entire path that may be provided
+  //Get name of file only--not the entire path that may be provided
   if ( pos != std::string::npos ) {
     str = name.substr(pos+1);
   } 
@@ -16,13 +13,26 @@ void GetTreeName( TString inputFile="") {
     str = name;
   }
 
-  //Find the .root and cut it out..
-  size_t und = str.find("v68");
+  //Find the .root and cut it out.
+  //It must be done this way since the usual names here are: reducedTree.blah.Name_of_Dataset_v69_batch_1234.root
+  size_t und = str.find("v65");
   if ( und > 255 ) und = str.find("v66");
   if ( und > 255 ) und = str.find("v67");
+  if ( und > 255 ) und = str.find("v68");
   if ( und > 255 ) und = str.find("v69");
   if ( und > 255 ) und = str.find("v70");
+  if ( und > 255 ) und = str.find("v71");
+  if ( und > 255 ) und = str.find("v71s");
+  if ( und > 255 ) und = str.find("v72");
+  if ( und > 255 ) und = str.find("v73");
+  if ( und > 255 ) und = str.find("v74");
   string ret = str.substr(0,und+3);
+
+  //Special for v71 skim&slim
+  if ( str.find("ra2b") < 256 ) {
+    und = str.find("v71s");
+    ret = str.substr(0,und+4);
+  }
 
   cout << ret << endl;
   return;
