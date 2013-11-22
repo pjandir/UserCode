b8b7def0546e74bb98d6faca81f9740d13152eee
Pawandeep <pjand001@ucr.edu>
2013-11-22 05:45:56 -0800

    add sample names

--- split/GetSampleName.C
+++ split/GetSampleName.C
@@ -79,6 +79,9 @@ void GetSampleName( TString inputFile="") {
   if (inputFile.Contains("T6bbHH"))           ret = "T6bbHH"  ;
   if (inputFile.Contains("T6ttHH"))           ret = "T6ttHH"  ;
   if (inputFile.Contains("T5Wh"))             ret = "T5Wh"    ;
+  if (inputFile.Contains("T1tbbb"))           ret = "T1tbbb"  ;
+  if (inputFile.Contains("T1tttb"))           ret = "T1tttb"  ;
+  if (inputFile.Contains("T2tb"))             ret = "T2tb"    ;
   //pMSSM
   if (inputFile.Contains("pMSSM")) {
     if ( inputFile.Contains("batch1") )     ret = "pMSSM_b1";
